9426654530448d5016ac21eb8e24cdc6f95bf522
nguyenhuunhan23 <43365682+nguyenhuunhan23@users.noreply.github.com>
2018-09-24 23:29:21 +0700

    try using another method to read

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -10,6 +10,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,26 +29,24 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      */
+    public static List<String> readFileInList(String fileName) throws IOException {
+
+        List<String> lines = Collections.emptyList();
+        lines
+                = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8); // do something
+        return lines;
+    }
+
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         System.out.println("Hello World This is TDTU");
-        File f = new File("data.txt"); // data
-        BufferedReader br = null;
         int counter = 0; // counter to count number of students
 
-        try {
-            br = new BufferedReader(new FileReader(f));
-            String line;
-            while ((line = br.readLine()) != null) { // read line by line
-                System.out.println("Hello World I am " + line); // print name after reading line
-                ++counter;
-            }
-        } catch (Exception e) {
-            e.printStackTrace(); // file not found , null pointer , tec
-        } finally {
-            if (br != null) {
-                br.close(); // close when finish reading
-            }
+        List l = readFileInList("data.txt");
+        Iterator<String> itr = l.iterator();
+        while (itr.hasNext()) {
+            ++counter;
+            System.out.println("Hello World I am " + itr.next());
         }
         System.out.println("Total " + counter + " Students");
 
